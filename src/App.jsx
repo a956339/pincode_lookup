35676974a0e626690d099cefe84067914d353227
a956339 <144869760+a956339@users.noreply.github.com>
2024-09-28 12:27:13 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PincodeLookup from "./Component/PincodeLookup";
+
+const App = () => {
+  return (
+    <div>
+      <PincodeLookup />
+    </div>
+  );
+};
+
+export default App;
